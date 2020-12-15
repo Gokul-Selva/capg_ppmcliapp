06339ac35188d403052696b7103f9ce11b3da51b
gokul <gokulselvakm3@gmail.com>
2020-12-15 22:38:54 +0530

    Header and Project component with UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
 import HeaderComponent from './components/layout/HeaderComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import ProejctItemComponent from './ProjectItemComponent'
 
 class Dashboard extends React.Component {
     render(){
         return(
-            <h1>Dashboard Component</h1>
-        );
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                            Create a Project
+                        </a>
+                        <br />
+                        <hr />
+    
+                       <ProjectItemComponent/>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
     }
 }
 export default Dashboard;
